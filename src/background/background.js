ecaec4a0263b1aafbff181957a90c97f69896ac7
Florian Jourda <florian.jourda@gmail.com>
2015-04-21 21:43:39 -0700

    Post scraped contact to Airtable contact table

--- src/background/background.js
+++ src/background/background.js
@@ -1,11 +1,11 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+// if you checked 'fancy-settings' in extensionizr.com, uncomment this lines
 
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
+// var settings = new Store('settings', {
+//     'sample_setting': 'This is how you use Store.js to remember values'
 // });
 
 // Saves data that artoo has scraped on the page
-var scrapedData;
+var linkedInContact;
 
 // Listen to the content script
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
@@ -14,7 +14,7 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
         // Show page action icon in address bar
         chrome.pageAction.show(sender.tab.id);
         // Save to send later if the user clicks on the page action
-        scrapedData = request.scrapedData;
+        linkedInContact = request.scrapedData;
         sendResponse('ok');
     } else {
         sendResponse('error');
@@ -25,4 +25,29 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 chrome.pageAction.onClicked.addListener(function(tab) {
     console.log('CLICKED');
     // Do something
+
+    $.ajax({
+        method: 'POST',
+        url: 'https://api.airtable.com/v0/appFkPIUEb8ApIYqf/Contacts',
+        headers: {'Authorization': 'Bearer keyUSUFogZHq7YcwU'},
+        data: {
+          fields: {
+            'Contact Name': linkedInContact.name,
+            'Current Job': linkedInContact.title,
+            'Functions': [],
+//            'Locations': [linkedInContact.location],
+//            'Industries': [linkedInContact.industry],
+            'LinkedIn Profile': 'TODO',
+            'Networking Meetings': [],
+            'Picture': []
+          }
+        },
+        dataType: 'json',
+        success: function(response) {
+            console.log('success', response);
+        },
+        error: function(response) {
+            console.error('error', response);
+        }
+    });
 });
